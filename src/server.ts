3347c84005e5ea8e3e33be92a5f25a39b20d39f0
Dorothy Ren <dorothyxren@gmail.com>
2024-06-19 18:58:12 -0400

    completed GET movie by Id and search

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,3 @@ import app from "./app";
 const PORT: Number = 5050;
 
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
-
--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -1,30 +1,67 @@
 import supertest from "supertest";
+
+// now we can import express (and prisma client)
 import app from "../app";
-import { prismaMock } from "./testclient";
 
+import { prismaMock } from "../lib/singleton";
 
-const fakemovies =
-    [{
-        id: "1234567",
-        title: "fake title",
-        description: "fake description"
-    }]
+const fakeMovie = {
+    id: "345897",
+    title: "Fakery",
+    description: "The phoniest movie ever",
+}
 
+const newMovie = {
+    id: "5645645",
+    title: "New Movie",
+    description: "I'm new",
+}
 
-prismaMock.movie.findMany.mockResolvedValue(fakemovies)
+const fakeMovies = [
+    fakeMovie,
+    newMovie,
+    {
+        id: "3453453",
+        title: "The Craziest Movie Ever",
+        description: "super super crazy",
+    },
+]
 
-describe("Our five movie API routes", () => {
-    test("get movies", async () => {
-        //make a get request to movies
-        const moviesresult = await supertest(app).get("/movies");
-        // to do:insert some actual data to test
-        expect(moviesresult.status).toEqual(200);
-        expect(moviesresult.body).toEqual(fakemovies);
-        //make a get request to movies with a search query param
-        const searchresults = await supertest(app).get("/movies?search=inc")
-        //to do: insert actual data
-        expect(searchresults.status).toEqual(200);
-        expect(searchresults.body).toEqual(fakemovies)
+describe("Our five movie api routes", () => {
+    test("Get movies", async () => {
+        // mocking the return value of prisma movies
+        prismaMock.movie.findMany.mockResolvedValue(fakeMovies)
+
+        //make a get request to movies 
+        const movieRes = await supertest(app).get("/movies");
+
+        //todo: insert some actual data to test
+        expect(movieRes.status).toEqual(200)
+        expect(movieRes.body).toEqual(fakeMovies)
 
     })
+
+    // todo - don't skip me
+    test("Search Movies", async () => {
+        prismaMock.movie.findMany.mockResolvedValue(fakeMovies.filter(movie => movie.title.includes("Fake")));
+        const searchRes = await supertest(app).get("/movies?title=Fake");
+        expect(searchRes.status).toEqual(200);
+        expect(searchRes.body).toEqual([fakeMovie]); // Assuming these two contain "Fake"
+    })
+
+
+
+    test("Get movie by id", async () => {
+        prismaMock.movie.findUnique.mockResolvedValue(fakeMovie)
+
+        //make a get req to movies by searching id
+        const movieById = await supertest(app).get("/movies/345897")
+
+        //todo: insert some actual data to test
+        expect(movieById.status).toEqual(200)
+        expect(movieById.body).toEqual(fakeMovie)
+
+    })
+
 })
+
